e35a4732d9be7353173a3128d17cfef115c5ece9
Lucia Rocha <luciarocha@imacdelu.local>
2023-03-29 18:10:31 -0300

    app.module.ts y app component html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Alumnos } from './models/alumnos.model';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'componentesLuciaRocha';
+ 
+//"Lucia", 'Roma', 'Marta', 'Maria', 'Antonio'
+
+  public alumnos: Alumnos[] = [
+    new Alumnos (1, 37, 'Lucia', 'Rocha', 'Arquitectura' ),
+    new Alumnos (2, 37, 'Roma', 'Martinelli', 'Arquitectura' ),
+    new Alumnos (3, 37, 'Maria', 'Callas', 'Arquitectura' ),
+    new Alumnos (4, 37, 'Antonio', 'Banderas', 'Arquitectura' )
+  
+  ];
+
 }
