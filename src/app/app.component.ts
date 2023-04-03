69e43a8f1370149dc92ffa25009f1403d42bba30
Lucia Rocha <luciarocha@imacdelu.local>
2023-04-03 18:40:14 -0300

    volver a desafio 01, se modificaron y eliminaron archivos del desafío 02

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,13 @@
 import { Component } from '@angular/core';
-import { Alumnos } from './models/alumnos.model';
 
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
-export class AppComponent {
- 
-//"Lucia", 'Roma', 'Marta', 'Maria', 'Antonio'
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
+  })
+  export class AppComponent {
 
-  public alumnos: Alumnos[] = [
-    new Alumnos (1, 37, 'Lucia', 'Rocha', 'Arquitectura' ),
-    new Alumnos (2, 37, 'Roma', 'Martinelli', 'Arquitectura' ),
-    new Alumnos (3, 37, 'Maria', 'Callas', 'Arquitectura' ),
-    new Alumnos (4, 37, 'Antonio', 'Banderas', 'Arquitectura' )
-  
-  ];
+  title = 'componentesLuciaRocha';
 
 }
--- src/app/models/alumnos.model.ts
+++
@@ -1,9 +0,0 @@
-export class Alumnos {
-    constructor(
-        public nicname: number, 
-        public edad: number,
-        public nombre: string,
-        public apellido: string,
-        public carrera: string
-    ) { }
-}
